ac631ae51820fdf56f7a0b4d1f1b1d3304481c8e
BisweshD <122183419+BisweshD@users.noreply.github.com>
2023-01-11 22:10:28 -0500

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -279,7 +279,7 @@ const contactInfo = {
 
 
 
-const isHireable = True; // Set false if you are not looking for a job. Also isHireable will be display as Open for opportunities: Yes/No in the GitHub footer
+const isHireable = true; // Set false if you are not looking for a job. Also isHireable will be display as Open for opportunities: Yes/No in the GitHub footer
 
 export {
   illustration,
